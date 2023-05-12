f723b2aa9cbc00980474e7518e0b35be7869b8aa
Admin <Admin@TEB127006>
2023-05-12 08:49:33 +0200

    Add project files.

---
+++ Metoder Uppgift 6.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Störst(6, 2);
+    }
+
+    static void Störst(int tal1, int tal2)
+    {
+        int StörstTal=0;
+        if (tal1>tal2)
+            StörstTal = tal1;
+        else if (tal1<tal2)
+            StörstTal=tal2;
+        Console.WriteLine(StörstTal);
+        
+    }
+}
